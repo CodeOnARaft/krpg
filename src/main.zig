9c02bf8d0bb4454252edaf595b240f328f53f6b0
CodeOnARaft <kevhann@gmail.com>
2025-02-11 21:57:28 -0500

    3D World

--- src/main.zig
+++ src/main.zig
@@ -1,16 +1,48 @@
 // raylib-zig (c) Nikolas Wipper 2023
 
 const rl = @import("raylib");
+const MAX_COLUMNS = 20;
 
 pub fn main() anyerror!void {
     // Initialization
     //--------------------------------------------------------------------------------------
-    const screenWidth = 800;
-    const screenHeight = 450;
+    const screenWidth = 1280;
+    const screenHeight = 720;
 
-    rl.initWindow(screenWidth, screenHeight, "raylib-zig [core] example - basic window");
+    rl.initWindow(screenWidth, screenHeight, "krpg");
     defer rl.closeWindow(); // Close window and OpenGL context
 
+    rl.setTargetFPS(60); // Set our game to run at 60 frames-per-second
+    rl.toggleFullscreen();
+
+    var camera = rl.Camera3D{
+        .position = rl.Vector3.init(4, 2, 4),
+        .target = rl.Vector3.init(0, 1.8, 0),
+        .up = rl.Vector3.init(0, 1, 0),
+        .fovy = 60,
+        .projection = .perspective,
+    };
+
+    var heights: [MAX_COLUMNS]f32 = undefined;
+    var positions: [MAX_COLUMNS]rl.Vector3 = undefined;
+    var colors: [MAX_COLUMNS]rl.Color = undefined;
+
+    for (0..heights.len) |i| {
+        heights[i] = @as(f32, @floatFromInt(rl.getRandomValue(1, 12)));
+        positions[i] = rl.Vector3.init(
+            @as(f32, @floatFromInt(rl.getRandomValue(-15, 15))),
+            heights[i] / 2.0,
+            @as(f32, @floatFromInt(rl.getRandomValue(-15, 15))),
+        );
+        colors[i] = rl.Color.init(
+            @as(u8, @intCast(rl.getRandomValue(20, 255))),
+            @as(u8, @intCast(rl.getRandomValue(10, 55))),
+            30,
+            255,
+        );
+    }
+
+    rl.disableCursor(); // Limit cursor to relative movement inside the window
     rl.setTargetFPS(60); // Set our game to run at 60 frames-per-second
     //--------------------------------------------------------------------------------------
 
@@ -18,7 +50,7 @@ pub fn main() anyerror!void {
     while (!rl.windowShouldClose()) { // Detect window close button or ESC key
         // Update
         //----------------------------------------------------------------------------------
-        // TODO: Update your variables here
+        camera.update(.first_person);
         //----------------------------------------------------------------------------------
 
         // Draw
@@ -26,9 +58,31 @@ pub fn main() anyerror!void {
         rl.beginDrawing();
         defer rl.endDrawing();
 
-        rl.clearBackground(rl.Color.white);
+        rl.clearBackground(rl.Color.ray_white);
 
-        rl.drawText("Congrats! You created your first window!", 190, 200, 20, rl.Color.light_gray);
+        {
+            camera.begin();
+            defer camera.end();
+
+            // Draw ground
+            rl.drawPlane(rl.Vector3.init(0, 0, 0), rl.Vector2.init(32, 32), rl.Color.light_gray);
+            rl.drawCube(rl.Vector3.init(-16.0, 2.5, 0.0), 1.0, 5.0, 32.0, rl.Color.blue); // Draw a blue wall
+            rl.drawCube(rl.Vector3.init(16.0, 2.5, 0.0), 1.0, 5.0, 32.0, rl.Color.lime); // Draw a green wall
+            rl.drawCube(rl.Vector3.init(0.0, 2.5, 16.0), 32.0, 5.0, 1.0, rl.Color.gold); // Draw a yellow wall
+
+            // Draw some cubes around
+            for (heights, 0..) |height, i| {
+                rl.drawCube(positions[i], 2.0, height, 2.0, colors[i]);
+                rl.drawCubeWires(positions[i], 2.0, height, 2.0, rl.Color.maroon);
+            }
+        }
+
+        rl.drawRectangle(10, 10, 220, 70, rl.Color.sky_blue.fade(0.5));
+        rl.drawRectangleLines(10, 10, 220, 70, rl.Color.blue);
+
+        rl.drawText("First person camera default controls:", 20, 20, 10, rl.Color.black);
+        rl.drawText("- Move with keys: W, A, S, D", 40, 40, 10, rl.Color.dark_gray);
+        rl.drawText("- Mouse move to look around", 40, 60, 10, rl.Color.dark_gray);
         //----------------------------------------------------------------------------------
     }
 }
